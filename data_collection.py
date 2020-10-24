668400756239b849e1ca4bf58e17289f66f12ccf
Shabix <kmharouna@gmail.com>
2020-10-24 12:24:23 +0300

    queried the data csv file in the cleaning script

--- data_collection.py
+++ data_collection.py
@@ -10,7 +10,7 @@ BASE_DIR = os.path.dirname(this_file)
 
 path = '/usr/bin/chromedriver/chromedriver'
 
-df = gs.get_jobs('data scientist',15, False, path, 15)
+df = gs.get_jobs('data scientist',1000, False, path, 15)
 path = os.path.join(BASE_DIR, "data")
 os.makedirs(path, exist_ok=True)
 filepath = os.path.join("data", "glassdor_jobs.csv")
